31f8f8a28c11026a31733e0d204350462668d633
wahabbhai9571 <dilkhushverma8824@gmail.com>
2025-07-20 12:03:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", ""))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", ""))
+WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
+AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", None)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
